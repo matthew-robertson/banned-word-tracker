e0e28a6b14e9aa130405f76e5c339fcca78faa66
matthew-robertson <m33rober@uwaterloo.ca>
2017-09-12 23:35:40 -0400

    Adds support for detection of pluralized vore, as well as accented characters.

--- bot.py
+++ bot.py
@@ -5,7 +5,7 @@ import datetime
 import math
 
 # A pattern to match the word vore, and only the single word vore.
-pattern = re.compile(r'\b[V|v][O|o][R|r][E|e]\b')
+pattern = re.compile(r'\b[V|v][O|Ò|Ó|Ô|Õ|Ö|o|ò|ó|ô|õ|ö][R|r][E|È|É|Ê|Ë|e|è|é|ê|ë][S|s]?\b')
 serverAndDate = {}
 botStartup = datetime.datetime.now()
 lastMention = datetime.datetime.now() - datetime.timedelta(days=1)
