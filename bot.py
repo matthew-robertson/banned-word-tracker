db64797d48d88950ac4931d681ba178bce96e780
matthew-robertson <m33rober@uwaterloo.ca>
2018-10-06 16:44:16 -0400

    Bot now triggers on Edits
    Exactly what it says on the tin. This'll need to be cleaned up as part
    of the upcoming refactor, since I've literally copied and pasted the
    processing function for now.

--- bot.py
+++ bot.py
@@ -31,7 +31,6 @@ def writeTimesToFile():
         for serverId in serverAndDate:
             target.write('{},{},{}\n'.format(serverId, serverAndDate[serverId].strftime("%Y-%m-%d %H:%M:%S"), awake[serverId]))
 
-
 @client.event
 async def on_ready():
     print('Logged in as')
@@ -40,24 +39,14 @@ async def on_ready():
     print('------')
 
 @client.event
-async def on_message(message):
+async def on_message_edit(before, message):
     global botStartup
     global serverAndDate
     global lastMention
     global awake
     currentTime = datetime.datetime.now()
     serverId = message.server.id
-    
-    ''' I'm just using startup time instead of join date, because that makes way more sense for older
-        servers if the filesystem stuff doesn't work
-    '''
-    #bot = None
-    #for x in message.server.members:
-    #    if client.user.id == x.id:
-    #        bot = x
-    #        break
 
-    # Timezone hack, apparently isn't needed for Heroku.
     lastReferenced = botStartup
     if serverId in serverAndDate:
         lastReferenced = serverAndDate[serverId]
@@ -126,6 +115,82 @@ async def on_message(message):
             await client.send_message(message.channel, '{} referenced the forbidden word, setting the counter back to 0. I\'ll wait a half hour before warning you again.\n The server went {}{}{}{} without mentioning it.'.format(message.author.mention, dt, ht, mt, st))
             lastMention[serverId] = currentTime
 
+@client.event
+async def on_message(message):
+    global botStartup
+    global serverAndDate
+    global lastMention
+    global awake
+    currentTime = datetime.datetime.now()
+    serverId = message.server.id
+
+    lastReferenced = botStartup
+    if serverId in serverAndDate:
+        lastReferenced = serverAndDate[serverId]
+    if serverId not in lastMention:
+        bot = None
+        for x in message.server.members:
+            if client.user.id == x.id:
+                bot = x
+                break
+        lastMention[serverId] = bot.joined_at
+    if serverId not in awake:
+        awake[serverId] = True
+
+    # Begin time formatting
+    diff = currentTime - lastReferenced
+    hours = math.floor(diff.seconds/3600)
+    minutes = math.floor((diff.seconds - hours * 3600)/60)
+    seconds = diff.seconds - hours * 3600 - minutes * 60
+    dt = "{} days, ".format(diff.days)
+    ht = "{} hours, ".format(hours)
+    mt = "{} minutes, and ".format(minutes)
+    st = "{} seconds".format(seconds)
+
+    if diff.days == 1:
+        dt = "1 day, "
+    elif diff.days == 0:
+        dt = ""
+        if hours == 0:
+            ht = ""
+            mt = "{} minutes and ".format(minutes)
+            if minutes == 0:
+                mt = ""
+
+    if hours == 1:
+        ht = "1 hour, "
+    if minutes == 1:
+        if ht == "":
+            mt = "1 minute and "
+        else:
+            mt = "1 minute, and "
+    if seconds == 1:
+        st = "1 second"
+    # End Time formatting stuff
+
+    if (hasattr(message.author, 'server_permissions')):
+        permission = message.author.server_permissions
+        if message.content.startswith('!vtsilence') and permission.administrator:
+            await client.send_message(message.channel, "Ok {}, I'll be quiet now. use '!vtalert' to wake me back up!".format(message.author.mention))
+            awake[serverId] = False
+            writeTimesToFile()
+        elif message.content.startswith('!vtalert') and permission.administrator:
+            await client.send_message(message.channel, "Ok {}, I'm scanning now.".format(message.author.mention))
+            awake[serverId] = True
+            writeTimesToFile()
+    if message.content.startswith('!vtsilence') or message.content.startswith('!vtalert'):
+        pass
+    elif message.content.startswith('!vthelp'):
+        await client.send_message(message.channel, "You can ask me how long we've made it with '!vt'.\n If you're an admin you can silence me with '!vtsilence' and wake me back up with '!vtalert'")
+    elif message.content.startswith('!vt'):
+        await client.send_message(message.channel, 'The server has gone {}{}{}{} without mentioning the forbidden word.'.format(dt, ht, mt, st))
+    if ((pattern.search(message.content) is not None) and (message.author.id != client.user.id)):
+        serverAndDate[serverId] = currentTime
+        writeTimesToFile()
+        print ("{}::: {} lasted {} seconds.".format(currentTime, serverId, (currentTime - lastMention[serverId]).total_seconds()))
+        if (awake[serverId] and (currentTime - lastMention[serverId]).total_seconds() >= 1800):
+            await client.send_message(message.channel, '{} referenced the forbidden word, setting the counter back to 0. I\'ll wait a half hour before warning you again.\n The server went {}{}{}{} without mentioning it.'.format(message.author.mention, dt, ht, mt, st))
+            lastMention[serverId] = currentTime
 
 readTimesFromFile()
 print('Stored server info:')
