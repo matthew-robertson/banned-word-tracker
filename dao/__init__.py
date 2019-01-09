6aebc4ba69af1c9a6e01abb2e7e81043e6b37f80
Nathan Woodger <woodgern@gmail.com>
2019-01-08 23:26:19 -0500

    Remove specific database location

--- dao/__init__.py
+++ dao/__init__.py
@@ -3,6 +3,6 @@ from sqlite3 import Error
 
 # When a configuration file becomes a thing, the db location should 100% go there
 try:
-  conn = sqlite3.connect('/home/njwoodge/Documents/tracker.db')
+  conn = sqlite3.connect('/location/of/database/tracker.db')
 except Error as e:
   print(e)
