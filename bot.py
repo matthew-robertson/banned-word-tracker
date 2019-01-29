3ee4f37dfab85d3bba837443a1493ede7b65e51e
Nathan Woodger <woodgern@gmail.com>
2019-01-28 19:42:03 -0500

    Add configuration file (#25)
    Add `main.py` to load configs and start bot
    
    Lightly rework `bot.py` in order to be started as a function

--- bot.py
+++ bot.py
@@ -4,14 +4,12 @@ import discord
 from enum import Enum
 import math
 import re
-import sys
 import time
 
 from dao.server_dao import ServerDao
 
 # A pattern to match the word vore, and only the single word vore.
 pattern = re.compile(r'\b[\*|_|~|`|-|\.]*[V|v][\*|_|~|`|-|\.]*[O|Ò|Ó|Ô|Õ|Ö|o|ò|ó|ô|õ|ö|ᴑ|о][\*|_|~|`|-|\.]*[R|r][\*|_|~|`|-|\.]*[E|È|É|Ê|Ë|Е|e|è|é|ê|ë|е][\*|_|~|`|-|\.]*[S|s]?\b')
-client = discord.Client(shard_id=int(sys.argv[1]), shard_count=int(sys.argv[2]))
 
 class Commands(Enum):
     NOCOMMAND = 0
@@ -70,14 +68,14 @@ def parse_for_command(msg, msg_author):
     elif msg.startswith('!vt'):
         return Commands.VT
 
-def handle_message(message, botID):
+def handle_message(server_dao, message, botID):
     msg_to_send = False
     currentTime = datetime.datetime.now()
     if not message.server:
         return msg_to_send
 
     serverId = message.server.id
-    currentServer = ServerDao.get_server(serverId)
+    currentServer = server_dao.get_server(serverId)
     if currentServer is None:
         currentServer = {}
         currentServer['server_id'] = serverId
@@ -89,7 +87,7 @@ def handle_message(message, botID):
         currentServer['infracted_at'] = bot.joined_at
         currentServer['calledout_at'] = bot.joined_at
         currentServer['awake'] = True
-        ServerDao.insert_server(currentServer)
+        server_dao.insert_server(currentServer)
 
     timeLasted = format_time(currentTime, currentServer['infracted_at'])
 
@@ -97,11 +95,11 @@ def handle_message(message, botID):
     if foundCommand is Commands.VTSILENCE:
         msg_to_send = "Ok {}, I'll be quiet now. use '!vtalert' to wake me back up!".format(message.author.mention)
         currentServer['awake'] = False
-        ServerDao.insert_server(currentServer)
+        server_dao.insert_server(currentServer)
     elif foundCommand is Commands.VTALERT:
         msg_to_send = "Ok {}, I'm scanning now.".format(message.author.mention)
         currentServer['awake'] = True
-        ServerDao.insert_server(currentServer)
+        server_dao.insert_server(currentServer)
     elif foundCommand is Commands.VTHELP:
         msg_to_send = "You can ask me how long we've made it with '!vt'.\n If you're an admin you can silence me with '!vtsilence' and wake me back up with '!vtalert'"
     elif foundCommand is Commands.VT:
@@ -115,35 +113,32 @@ def handle_message(message, botID):
             currentServer['calledout_at'] = currentTime
             msg_to_send = "{} referenced the forbidden word, setting the counter back to 0. I'll wait a half hour before warning you again.\n The server went {} without mentioning it.".format(message.author.mention, timeLasted)
 
-        ServerDao.insert_server(currentServer)
+        server_dao.insert_server(currentServer)
         print("{}::: {} lasted {} seconds.".format(currentTime, serverId, (currentTime - currentServer['infracted_at']).total_seconds()))
 
     return msg_to_send
 
-@client.event
-async def on_ready():
-    print("Logged in as")
-    print(client.user.name)
-    print(client.user.id)
-    print("------")
+def run_bot(conn, shard_id, shard_count, client_key):
+    client = discord.Client(shard_id=shard_id, shard_count=shard_count)
+    server_dao = ServerDao(conn)
 
-@client.event
-async def on_message_edit(before, message):
-    msg_to_send = handle_message(message, client.user.id)    
-    if msg_to_send:
-        await client.send_message(message.channel, msg_to_send)
+    @client.event
+    async def on_ready():
+        print("Logged in as")
+        print(client.user.name)
+        print(client.user.id)
+        print("------")
 
-@client.event
-async def on_message(message):
-    msg_to_send = handle_message(message, client.user.id)    
-    if msg_to_send:
-        await client.send_message(message.channel, msg_to_send)
+    @client.event
+    async def on_message_edit(before, message):
+        msg_to_send = handle_message(server_dao, message, client.user.id)    
+        if msg_to_send:
+            await client.send_message(message.channel, msg_to_send)
 
-while True:
-    try:
-        with open("key.txt", "r") as target:
-            for line in target:
-                line = line.strip()
-                client.run(line)
-    except BaseException:
-            time.sleep(5)
+    @client.event
+    async def on_message(message):
+        msg_to_send = handle_message(server_dao, message, client.user.id)    
+        if msg_to_send:
+            await client.send_message(message.channel, msg_to_send)
+
+    client.run(client_key)
