1795cf48cd63dfe796df5806836b36a23b92c072
Matthew Robertson <matthew.d.robertson.95@gmail.com>
2019-10-17 19:45:06 -0400

    Discord API requests use Authorization header now (#51)
    Turns out, the reason the startup script wasn't working is that the API
    requires an authorization header now, instead of attaching the token as
    a param.

--- startup.py
+++ startup.py
@@ -6,8 +6,8 @@ import config
 print('Hitting discord\'s API to determine how many shards are needed.')
 key = config.CLIENT_KEY
 
-url = 'https://discordapp.com/api/v6/gateway/bot?token=' + key
-response = requests.get(url)
+url = 'https://discordapp.com/api/v6/gateway/bot'
+response = requests.get(url, headers = {'Authorization': 'Bot ' + key})
 
 processes = []
 if (response.ok):
@@ -16,9 +16,6 @@ if (response.ok):
 	for x in range(0, shard_count):
 		processes.append(subprocess.Popen('./botLaunch.sh '+str(x)+' ' + str(shard_count), shell=True))
 	print("Launched processes")
-	with open("logs/pids.txt", "w") as target:
-		for process in processes:
-			target.write('{}\n'.format(process.pid))
 else:
 	print("Can't reach the Gateway endpoint. Giving up.")
 
