fcabb8cc55b2ba8129cfc25c6f1927cf735a64ff
matthew-robertson <m33rober@uwaterloo.ca>
2019-10-23 22:34:23 -0400

    Refactors from ServerDao and dicts to classes
    A new Server class has been added to handle its functionality, as has a
    Ban class. This intends to factor out a significant portion of bot.py

---
+++ serverobjects/server.py
@@ -0,0 +1,32 @@
+import requests
+import json
+
+from config import API_BASE_URL, CLIENT_KEY
+from serverobjects.ban import BanInstance
+
+class DiscordServer:
+	def __init__(self, server_data, current_time):
+		self.server_id = int(server_data['server_id'])
+		self.awake = bool(server_data['awake'])
+		self.timeout_duration_seconds = int(server_data['timeout_duration_seconds'])
+		self.banned_words = []
+		for word in server_data['banned_words']:
+			ban = BanInstance(word, current_time, self.timeout_duration_seconds)
+			self.banned_words.append(ban)
+
+	def set_awake(self, new_awake):
+		self.update_server_settings({'awake': new_awake})
+
+	def set_timeout(self, new_timeout):
+		self.update_server_settings({'timeout_duration_seconds': new_timeout})
+
+	def update_server_settings(self, updated_params):
+		response = requests.post(
+			API_BASE_URL + 'v1/servers/' + str(self.server_id), 
+			headers = {'Authorization': 'Bot ' + CLIENT_KEY},
+			json = updated_params)
+
+		if (response.ok):
+			jData = json.loads(response.content)
+			self.awake = bool(jData['awake'])
+			self.timeout_duration_seconds = int(jData['timeout_duration_seconds'])
