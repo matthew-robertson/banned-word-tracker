c555c52c6a4620baccbca4792c9ce3bf1fbc6eb5
matthew-robertson <m33rober@uwaterloo.ca>
2019-11-12 20:11:43 -0500

    Updates Server link
    Linking to the rules page is a little silly. I link to the about page
    now.

--- bot.py
+++ bot.py
@@ -162,7 +162,7 @@ def handle_vtct(current_time, current_server):
 def handle_vthelp():
     msg_to_send = "You can ask me how long we've made it with '!vt'.\n"
     msg_to_send += "You can learn how long my timeout is set for, and when I can issue another warning with '!vtct'.\n"
-    msg_to_send += "For other commands, server management, and general help, please check either the documentation (https://bwbdiscord.gitbook.io/banned-word-tracker/) or the support server (https://discord.gg/nUZsfYS)."
+    msg_to_send += "For other commands, server management, and general help, please check either the documentation (https://bwbdiscord.gitbook.io/banned-word-tracker/) or the support server (https://discord.gg/W7mrxRd)."
     return msg_to_send
 
 def handle_vtlast(current_time, current_server):
