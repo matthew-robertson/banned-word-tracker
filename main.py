1837e93704b4059e910b20eb137692f6b35326f7
matthew-robertson <m33rober@uwaterloo.ca>
2019-11-12 20:31:18 -0500

    Updates the session object
    Now it'll retry and exponentially back-off, which will hopefully help
    some things out, maybe.

--- main.py
+++ main.py
@@ -9,16 +9,16 @@ import bot
 def initialize_session():
     session = requests.Session()
     session.headers.update({'Authorization': 'Bot ' + config.CLIENT_KEY})
-    #retry = Retry(
-    #    total=retries,
-    #    read=retries,
-    #    connect=retries,
-    #    backoff_factor=backoff_factor,
-    #    status_forcelist=status_forcelist,
-    #)
-    #adapter = HTTPAdapter(max_retries=retry)
-    #session.mount('http://', adapter)
-    #session.mount('https://', adapter)
+    retry = Retry(
+        total=5,
+        read=1,
+        connect=5,
+        backoff_factor=0.3,
+        status_forcelist=[500],
+    )
+    adapter = HTTPAdapter(max_retries=retry)
+    session.mount('http://', adapter)
+    session.mount('https://', adapter)
     return session
 
 bot.run_bot(int(sys.argv[1]), int(sys.argv[2]), config.CLIENT_KEY, initialize_session())
