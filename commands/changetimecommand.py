7afd7a8cae0c1a0e8e59f4896db9d4037d7b9e88
matthew-robertson <m33rober@uwaterloo.ca>
2020-05-26 19:58:12 -0400

    Bot handles failure to set server params
    Now the bot will let the user know if it fails to update the delay,
    this is prep for fixing the max delay.

--- commands/changetimecommand.py
+++ commands/changetimecommand.py
@@ -9,7 +9,9 @@ class ChangeTimeCommand(Command):
 		parsed_time = parse_time(message[8:])
 
 		if parsed_time >= 0:
-			current_server.set_timeout(parsed_time)
+			update_succeeded = current_server.set_timeout(parsed_time)
+			if not update_succeeded:
+				return "Sorry, something went wrong and I couldn't update the delay. Make sure the delay is under 100 million seconds (~3 years)"
 			formatted_time = format_seconds(parsed_time)
 			return "Cool, from now on I'll wait at least {} between alerts.".format(formatted_time)
 		else:
